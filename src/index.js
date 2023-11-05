92f50ff8ea4bacd00518b3d6eaf81d8f5178e087
Artist149 <vasyl.kashapov@limestonedigital.com>
2023-11-05 23:59:34 +0100

    Add json storage

--- src/index.js
+++ src/index.js
@@ -2,33 +2,13 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-var users = {
-  list: function (req, res) {
-    res.send("user list");
-  },
+const films = require("./routes/films");
 
-  get: function (req, res) {
-    res.send("user " + req.params.uid);
-  },
+app.use("/static", express.static("./src/static/"));
 
-  delete: function (req, res) {
-    res.send("delete users");
-  },
-};
-
-// General
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-// User
-
-app.get("/users", users.list);
-app.get("/user/:uid", users.get);
-app.delete("/user", users.delete);
-
-// Posts
+// Films
+app.get("/films", films.getAll);
+app.get("/film/:fid", films.get);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
